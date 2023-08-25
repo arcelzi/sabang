ce34f3898ab728ea2d3503a2c760193249de72a4
arcelzi <siopet.45@gmail.com>
2023-08-25 10:33:33 +0700

    list purchase and fix

---
+++ lib/menu/list/listpurchase.dart
@@ -0,0 +1,20 @@
+
+
+class Purchases {
+  final int id;
+  final String tappers;
+  final int ph;
+  final int brix;
+  final int volume;
+  final int harga;
+  final DateTime date;
+
+  Purchases(
+      {required this.id,
+      required this.tappers,
+      required this.ph,
+      required this.brix,
+      required this.volume,
+      required this.harga,
+      required this.date});
+}
--- lib/menu/purchase.dart
+++ lib/menu/purchase.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:sabang/menu/addpurchase.dart';
+import '../menu/list/listpurchase.dart';
 
 class Purchase extends StatefulWidget {
   const Purchase({super.key});
@@ -12,69 +13,78 @@ class Purchase extends StatefulWidget {
 
 class _PurchaseState extends State<Purchase> {
   final String FontPoppins = 'FontPoppins';
+  final List<Purchases> purchases = [
+    Purchases(
+        id: 1,
+        tappers: 'Sabang',
+        ph: 2,
+        brix: 3,
+        volume: 5,
+        harga: 3000,
+        date: DateTime.now())
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFF5F6FB),
-      appBar: AppBar(
-        title: Text(
-          "Garden Control",
-          style: GoogleFonts.sourceSansPro(
-              fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
-        ),
-        leading: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              FontAwesomeIcons.angleLeft,
-              color: Colors.black,
-            )),
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            centerTitle: true,
-      ),
-      body: Padding(
-        padding: EdgeInsets.only(top: 56, left: 16, right: 16),
-        child: Container(
-          margin: EdgeInsets.symmetric(horizontal: 100),
-          height: 52,
-          width: 309,
-          child: GestureDetector(
-            onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-            child: TextFormField(
-              decoration: InputDecoration(
-                prefixIcon: Icon(
-                  Icons.search_rounded,
-                  color: Color(0xFFA9A9A9),
-                ),
-                filled: true,
-                fillColor: Color(0xFFE9E9E9),
-                hintText: 'Search Nira',
-                hintStyle: TextStyle(
-                    fontFamily: FontPoppins,
-                    fontSize: 14,
-                    color: Color(0xFFA9A9A9)),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide(color: Color(0xFFE9E9E9), width: 0),
-                ),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                    borderSide: BorderSide(color: Color(0xFFE9E9E9), width: 0)),
+        backgroundColor: Color(0xFFF5F6FB),
+        appBar: AppBar(
+          actions: [
+            Container(
+              width: 80,
+              child: IconButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: ((context) => AddPurchase())));
+                },
+                icon: Icon(FontAwesomeIcons.cartPlus, color: Colors.black, size: 25, ),
+                tooltip: "Add Purchase",
+                
               ),
-            ),
+            )
+          ],
+          title: Text(
+            "Purchase",
+            style: GoogleFonts.sourceSansPro(
+                fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
           ),
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(
+                FontAwesomeIcons.angleLeft,
+                color: Colors.black,
+              )),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
         ),
-         ),
-         floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: ((context) => AddPurchase ())));
-        },
-        backgroundColor: Color(0xFFE5E1E1),
-        child: Icon(FontAwesomeIcons.plus),
-        tooltip: 'Add Nira',
-        foregroundColor: Color(0xFFE0ADA2),
-         ),
-    );
+        body: ListView(
+          children: purchases.map((pu) {
+            return Card(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(pu.tappers),
+                        Text('PH : ' + pu.ph.toString()),
+                        Text('Brix : ' + pu.brix.toString()),
+                        Text('Volume : ' + pu.volume.toString()),
+                        Text('Harga : Rp.' + pu.harga.toString()),
+                        Text(pu.date.toString())
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            );
+          }).toList(),
+        ));
   }
 }
